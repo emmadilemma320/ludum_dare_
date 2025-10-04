3cdd57163f928d3763965d6bbd9cbff373b7324f
Lea Anderson-Young <lea.r.andersonyoung@gmail.com>
2025-10-03 17:28:59 -0700

    Added inventory and items
    Added item resource in a new script. Added new inventory script with the following methods: add_item, remove_item, get_max_capacity, is_full, and set_size.

---
+++ scripts/inventory.gd
@@ -0,0 +1,29 @@
+extends Node
+
+const INITIAL_SIZE = 10
+
+var capacity: int = 10
+var scalar: float = 1.0
+
+var items = []
+
+func add_item(new_item: item):
+	if is_full():
+		print("Inventory is full.")
+	else:
+		items.append(new_item)
+
+func remove_item(index: int) -> item:
+	var removed = items[index]
+	items.remove_at(index)
+	return removed
+
+func get_max_capacity():
+	return capacity
+
+func is_full():
+	return (len(items) == capacity)
+
+func set_size(scalar: float):
+	self.scalar = scalar
+	capacity = INITIAL_SIZE * scalar
---
+++ scripts/item.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name item
+
+var item_id: String = ""
+
+func _init(item_id: String) -> void:
+	self.item_id = item_id
