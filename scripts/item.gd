8ff0d63185193dca913407e4bde24d4f45198941
theDudk <michael.d.aspinall@icloud.com>
2025-10-04 18:44:02 -0700

    updated Item

--- scripts/item.gd
+++ scripts/item.gd
@@ -1,6 +1,10 @@
 class_name Item extends Resource
 
-@export var item_id: String
+enum Rarity {RUSTED, COPPER, SILVER, GOLD}
+
+@export var type: String
+@export var rarity: Rarity
+@export var texture: Texture2D
 
 func _to_string() -> String:
-	return item_id
+	return str(rarity) + type
