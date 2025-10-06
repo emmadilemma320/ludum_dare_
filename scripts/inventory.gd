35df058474beaaacf8869108430377290cb74b71
theDudk <michael.d.aspinall@icloud.com>
2025-10-05 22:51:37 -0700

    tweaks

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -1,8 +1,8 @@
 class_name Inventory extends Node
 
-const INITIAL_SIZE = 5
+const INITIAL_SIZE = 7
 
-var capacity: int = 5
+var capacity: int = 7
 var scalar: float = 1.0
 
 var items: Array[Item] = []
