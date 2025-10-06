fd6a2859d9c52521826467c34489dd377acaaa6d
theDudk <michael.d.aspinall@icloud.com>
2025-10-05 23:55:21 -0700

    updated you win

--- scripts/you_win_layer.gd
+++ scripts/you_win_layer.gd
@@ -1,14 +1,9 @@
 extends Control
 
-@onready var quit_button = $VBoxContainer/QuitButton
 
 func _ready():
 	visible = false  # hidden at start
-	quit_button.pressed.connect(_on_quit_pressed)
 
 # When player wins:
 func show_win_screen():
 	visible = true
-
-func _on_quit_pressed():
-	get_tree().quit()
